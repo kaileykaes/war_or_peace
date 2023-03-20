3866728279fc6c23cf87fb8d17f8487696170c99
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-20 15:21:16 -0600

    Test: turn types

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -11,10 +11,10 @@ RSpec.describe Turn do
     @card7 = Card.new(:heart, '3', 3)  
     @card8 = Card.new(:diamond, '2', 2)    
     @cards = [@card1, @card2, @card3]
-    @deck1 = Deck.new([card1, card2, card5, card8])   
-    @deck2 = Deck.new([card3, card4, card6, card7])   
-    @player1 = Player.new("Megan", deck1)    
-    @player2 = Player.new("Aurora", deck2)    
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])   
+    @deck2 = Deck.new([@card3, @card4, @card6, @card7])   
+    @player1 = Player.new("Megan", @deck1)    
+    @player2 = Player.new("Aurora", @deck2)    
     @turn = Turn.new(@player1, @player2)
   end
 
@@ -31,7 +31,27 @@ RSpec.describe Turn do
     expect(@turn.spoils_of_war).to eq([])
   end
 
-  xit '#turn_type' do 
-    expect(@turn.type).to eq(:basic)
+  describe '#turn type' do 
+    it 'turn type can be :basic'do
+      expect(@turn.type).to eq(:basic)
+    end
+
+    it 'turn type can be war' do 
+      @deck3 = Deck.new([@card1, @card2, @card5, @card8])
+      @deck4 = Deck.new([@card6, @card4, @card6, @card7])
+      @player3 = Player.new('Cameron', @deck3)
+      @player4 = Player.new('Elise', @deck4)
+      @turn2 = Turn.new(@player3, @player4)
+      expect(@turn2.type).to eq(:war)
+    end
+
+    xit 'turn type can be destruction' do 
+      @deck3 = Deck.new([@card1, @card3, @card2, @card5])
+      @deck4 = Deck.new([@card6, @card8, @card7, @card4])
+      @player3 = Player.new('Cameron', @deck3)
+      @player4 = Player.new('Elise', @deck4)
+      @turn3 = Turn.new(@player3, @player4)
+      expect(@turn3.type).to eq(:mutually_assured_destruction)
+    end
   end
 end
